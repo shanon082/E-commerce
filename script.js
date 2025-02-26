5fe7ae04cd183c0dca34dc8d96363cf314bfcb53
shanon cymon <shanonsimon082@gmail.com>
2025-02-26 15:57:05 +0300

    E-commerce project php

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let slideIndex = 0;
+let slideTimeout;
 showSlides();
 
 function showSlides() {
@@ -11,10 +12,11 @@ function showSlides() {
         slideIndex = 1;
     }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 10000); 
+    slideTimeout = setTimeout(showSlides, 10000); 
 }
 
 function changeSlide(n) {
+    clearTimeout(slideTimeout);
     slideIndex += n;
     let slides = document.getElementsByClassName("slide");
     if (slideIndex > slides.length) {
@@ -26,10 +28,40 @@ function changeSlide(n) {
         slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
+    slideTimeout = setTimeout(showSlides, 10000);
 }
 
+// let featuredSlideIndex = 0;
+// showFeaturedSlides();
 
-function toggleMenu() {
-    let navLinks = document.getElementById("nav-links");
-    navLinks.classList.toggle("show");
-}
+// function showFeaturedSlides() {
+//     let slides = document.getElementsByClassName("featured-slides")[0].children;
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     featuredSlideIndex++;
+//     if (featuredSlideIndex > slides.length) {
+//         featuredSlideIndex = 1;
+//     }
+//     slides[featuredSlideIndex - 1].style.display = "block";
+//     setTimeout(showFeaturedSlides, 10000);
+// }
+
+// function changeFeaturedSlide(n) {
+//     featuredSlideIndex += n;
+//     let slides = document.getElementsByClassName("featured-slides")[0].children;
+//     if (featuredSlideIndex > slides.length) {
+//         featuredSlideIndex = 1;
+//     } else if (featuredSlideIndex < 1) {
+//         featuredSlideIndex = slides.length;
+//     }
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slides[featuredSlideIndex - 1].style.display = "block";
+// }
+
+// function toggleMenu() {
+//     let navLinks = document.getElementById("nav-links");
+//     navLinks.classList.toggle("show");
+// }
