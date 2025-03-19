788359ffda364189bf5feb55cfb1fe20ef59ae46
shanon cymon <shanonsimon082@gmail.com>
2025-03-19 11:02:43 +0300

    added admin dashboard

--- admin/header.php
+++ admin/header.php
@@ -11,6 +11,14 @@
 
 <body>
     <header>
+        <div class="header">
+            <h1>TUKOLE <span>business</span></h1>
+            <div class="admin-initials">
+                <img src="" alt="">
+                <h2>Admin UserName</h2>
+            </div>
+        </div>
+        <div class="side-bar">
         <nav class="navbar">
             <ul class="nav-links">
                 <li><a href="index.php">Dashboard</a></li>
@@ -23,6 +31,7 @@
                 <li><a href="../login_and_signup/logout.php">Logout</a></li>
             </ul>
         </nav>
+        </div>
     </header>
 </body>
 
